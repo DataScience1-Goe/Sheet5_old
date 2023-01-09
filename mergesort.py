ffd15d93c828d5a098bb1736140943f8c6e94f5d
NikWeis <niklas.weiss@stud.uni-goettingen.de>
2023-01-09 12:54:53 +0100

    FUunktionsnamen geändert

--- mergesort.py
+++ mergesort.py
@@ -1,8 +1,8 @@
-def ASSIGNMENT(new_list, i, old_list, j):
+def assignment(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
+def merge_sort(list_to_sort_by_merge):
     if (
         len(list_to_sort_by_merge) > 1
         and not len(list_to_sort_by_merge) < 1
@@ -12,8 +12,8 @@ def mergeSort(list_to_sort_by_merge):
         left = list_to_sort_by_merge[:mid]
         right = list_to_sort_by_merge[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        merge_sort(left)
+        merge_sort(right)
 
         l = 0
         r = 0
@@ -21,10 +21,10 @@ def mergeSort(list_to_sort_by_merge):
 
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                assignment(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                assignment(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
                 r += 1
             i += 1
 
