26e896c6b401dd504e6c80d49135e9016f7d6748
NikWeis <niklas.weiss@stud.uni-goettingen.de>
2023-01-09 13:19:47 +0100

    Kommentare und Uebersichtlichkeitsaenderungen

--- mergesort.py
+++ mergesort.py
@@ -2,22 +2,25 @@ import matplotlib.pyplot as plt
 
 def assignment(new_list, i, old_list, j):
     new_list[i] = old_list[j]
-
+    return
 
 def merge_sort(list_to_sort):
-    if len(list_to_sort) > 1:
+    # teste of list_to_sort mindestens 2 Einträge hat (sonst ist sie schon sortiert)
+    if len(list_to_sort) > 1: 
+	# splitte die Liste in linken Teil und rechten Teil
         mid = len(list_to_sort) // 2
         left = list_to_sort[:mid]
         right = list_to_sort[mid:]
 
+	# Rekursiver Aufruf der Funktion um zunächst left und right sortiert zu haben
         merge_sort(left)
         merge_sort(right)
 
-        l = 0
-        r = 0
-        i = 0
+        l = 0; r = 0; i = 0
 
+	# Iteration durch left und right
         while l < len(left) and r < len(right):
+            # Schreibe immer den kleineren Wert von left und right in das ganze Array
             if left[l] <= right[r]:
                 assignment(new_list=list_to_sort, i=i, old_list=left, j=l)
                 l += 1
@@ -26,22 +29,26 @@ def merge_sort(list_to_sort):
                 r += 1
             i += 1
 
+	# Schreibe restliche Werte des linken Arrays in ganze Array
         while l < len(left):
             list_to_sort[i] = left[l]
             l += 1
             i += 1
 
+	# Schreibe restliche Werte des rechten Arrays in ganze Array
         while r < len(right):
             list_to_sort[i] = right[r]
             r += 1
             i += 1
     return
 
+# Plotte List ohne Sortieren
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
+
+# Plotte Liste mit Sortieren
 merge_sort(my_list)
-x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
